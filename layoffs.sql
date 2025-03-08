a05c2c2c5cc0c9c66cc2d8769302c87bfdfd28f8
linscot10 <linscot10@gmail.com>
2025-03-08 23:44:11 +0300

    cleaning data

--- layoffs.sql
+++ layoffs.sql
@@ -46,4 +46,19 @@ CREATE TABLE `layoffs_staging2` (
 
 
 INSERT INTO layoffs_staging2 
-SELECT * FROM layoffs
+SELECT * ,ROW_NUMBER() OVER(PARTITION BY company,location,industry,total_laid_off,percentage_laid_off,`date`,stage,country,funds_raised_millions) AS row_num
+FROM layoffs_staging;
+
+SELECT * FROM
+ layoffs_staging2 
+ WHERE row_num > 1
+ ;
+
+ DELETE  FROM
+ layoffs_staging2 
+ WHERE row_num > 1
+ ;
+
+
+SELECT * FROM
+ layoffs_staging2 ;
